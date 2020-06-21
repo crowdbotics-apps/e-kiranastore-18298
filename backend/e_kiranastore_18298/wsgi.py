723ebd342205594c816e9f2a8bd95b344a8c377c
Crowdbotics <team@crowdbotics.com>
2020-06-21 17:11:48 +0000

    Update models

--- backend/e_kiranastore_18298/wsgi.py
+++ backend/e_kiranastore_18298/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_kiranastore_18298.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "e_kiranastore_18298.settings")
 
 application = get_wsgi_application()
